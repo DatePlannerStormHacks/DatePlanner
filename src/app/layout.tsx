5f97bd24a9cdc4cfb371f261e4f6597cf758bb5a
mrq <mrq@sfu.ca>
2025-10-04 21:55:49 -0700

    adding header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,12 @@
-import type { Metadata } from 'next'
-import {
-  ClerkProvider,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from '@clerk/nextjs'
+import React from "react";
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+
+
+import "../../styles/Header.css";
+
 import { Geist, Geist_Mono } from 'next/font/google'
+import type { Metadata } from 'next'
 import './globals.css'
 
 const geistSans = Geist({
@@ -34,19 +33,6 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
-            <SignedOut>
-              <SignInButton />
-              <SignUpButton>
-                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                  Sign Up
-                </button>
-              </SignUpButton>
-            </SignedOut>
-            <SignedIn>
-              <UserButton />
-            </SignedIn>
-          </header>
           {children}
         </body>
       </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import DatePlanner from "./form";
+import Header from "../../components/Header";
 
 export default function Home() {
   return (
-    
     <div>
+      <Header />
       <main>
         <DatePlanner />
       </main>
